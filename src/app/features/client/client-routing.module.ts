bc8396c1ca087c00e435a40996a6a488ebf417e1
salma hamdi <saalmahm51@gmail.com>
2026-01-14 22:14:19 +0100

    Déclarer la route /client/withdraw vers WithdrawalComponent

--- src/app/features/client/client-routing.module.ts
+++ src/app/features/client/client-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientDashboardComponent } from './components/client-dashboard/client-dashboard.component';
 import { DepositComponent } from './components/deposit/deposit.component';
 import { ClientShellComponent } from './components/client-shell/client-shell.component';
+import { WithdrawalComponent } from './components/withdrawal/withdrawal.component';
 
 const routes: Routes = [
-   {
+  {
     path: '',
     component: ClientShellComponent,
     children: [
       { path: '', component: ClientDashboardComponent },
-      { path: 'deposit', component: DepositComponent }
+      { path: 'deposit', component: DepositComponent },
+      { path: 'withdraw', component: WithdrawalComponent }
     ]
   }
 ];
